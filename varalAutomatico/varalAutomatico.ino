200d28826bbd96c4b530c0ffdb067016da315a8d
Gardenial <114607879+Gardenial@users.noreply.github.com>
2023-04-27 14:28:31 -0300

    Update varalAutomatico.ino

--- varalAutomatico/varalAutomatico.ino
+++ varalAutomatico/varalAutomatico.ino
@@ -1,43 +1,50 @@
-#include <LiquidCrystal.h>
-#include <Servo.h>
+#include <LiquidCrystal.h> // Biblioteca para o display LCD
+#include <Servo.h> // Biblioteca para o micro servo
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-Servo myservo;
-
-const int sensorPin = A0;
-const int potPin = A1;
-const int servoPin = 9;
-
-int threshold = 500; // ajuste o limiar de umidade aqui
-int servoClosedAngle = 0; // ângulo em que o servo fica com o varal fechado
-int servoOpenAngle = 90; // ângulo em que o servo fica com o varal aberto
-int servoDelay = 5000; // tempo que o servo fica aberto antes de fechar novamente (em milissegundos)
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // Pinos utilizados para o display LCD
+Servo motor; // Objeto do micro servo
+int sensorPin = A0; // Pino utilizado para o sensor de umidade de solo
+int potPin = A1; // Pino utilizado para o potenciômetro
+int motorPin = 9; // Pino utilizado para o micro servo
+int motorPos = 0; // Posição inicial do motor
+int threshold = 500; // Limiar de umidade
+int timeOut = 5000; // Tempo em milissegundos que o motor permanece aberto
+unsigned long startTime = 0; // Tempo em que o motor foi acionado
+int umid = 0; // Variável para armazenar a umidade medida
+int potValue = 0; // Variável para armazenar o valor do potenciômetro
 
 void setup() {
-  lcd.begin(16, 2);
-  myservo.attach(servoPin);
-  pinMode(sensorPin, INPUT);
-  pinMode(potPin, INPUT);
+  lcd.begin(16, 2); // Inicializa o display LCD com 16 colunas e 2 linhas
+  motor.attach(motorPin); // Conecta o micro servo ao pino 9
 }
 
 void loop() {
-  int sensorValue = analogRead(sensorPin);
-  int potValue = analogRead(potPin);
-  int umidade = map(sensorValue, 0, 1023, 0, 100);
-  int delayTime = map(potValue, 0, 1023, 0, 10000);
+  umid = analogRead(sensorPin); // Lê a umidade medida pelo sensor
+  potValue = analogRead(potPin); // Lê o valor do potenciômetro
 
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Umidade: ");
-  lcd.print(umidade);
-  lcd.print("%");
+  // Converte o valor lido do potenciômetro em um intervalo de 0 a 180 graus
+  motorPos = map(potValue, 0, 1023, 0, 180);
 
-  if (umidade < threshold) {
-    myservo.write(servoOpenAngle);
-    delay(servoDelay);
-    myservo.write(servoClosedAngle);
+  lcd.clear(); // Limpa o display LCD
+  lcd.setCursor(0, 0); // Define o cursor para a primeira coluna da primeira linha
+  lcd.print("Umidade: "); // Escreve "Umidade: " no display LCD
+  lcd.print(umid); // Escreve a umidade medida no display LCD
+  lcd.setCursor(0, 1); // Define o cursor para a primeira coluna da segunda linha
+
+  // Verifica se a umidade é menor que o limiar definido
+  if (umid < threshold) {
+    motor.write(motorPos); // Gira o motor na direção horária
+    startTime = millis(); // Armazena o tempo atual
+    lcd.print("Motor Aberto"); // Escreve "Motor Aberto" no display LCD
   } else {
-    myservo.write(servoClosedAngle);
-    delay(delayTime);
+    motor.write(0); // Mantém o motor parado
+    lcd.print("Motor Fechado"); // Escreve "Motor Fechado" no display LCD
   }
+
+  // Verifica se o tempo em que o motor está aberto excedeu o tempo limite
+  if ((millis() - startTime) > timeOut) {
+    motor.write(0); // Gira o motor na direção anti-horária
+  }
+
+  delay(500); // Espera 500 milissegundos antes de executar novamente o loop
 }
