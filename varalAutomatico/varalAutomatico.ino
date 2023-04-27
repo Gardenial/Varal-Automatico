b1ad965b53d75b32200bd628413bb1fa78687b44
Gardenial <114607879+Gardenial@users.noreply.github.com>
2023-04-27 10:51:10 -0300

    Add files via upload

---
+++ varalAutomatico/varalAutomatico.ino
@@ -0,0 +1,43 @@
+#include <LiquidCrystal.h>
+#include <Servo.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+Servo myservo;
+
+const int sensorPin = A0;
+const int potPin = A1;
+const int servoPin = 9;
+
+int threshold = 500; // ajuste o limiar de umidade aqui
+int servoClosedAngle = 0; // ângulo em que o servo fica com o varal fechado
+int servoOpenAngle = 90; // ângulo em que o servo fica com o varal aberto
+int servoDelay = 5000; // tempo que o servo fica aberto antes de fechar novamente (em milissegundos)
+
+void setup() {
+  lcd.begin(16, 2);
+  myservo.attach(servoPin);
+  pinMode(sensorPin, INPUT);
+  pinMode(potPin, INPUT);
+}
+
+void loop() {
+  int sensorValue = analogRead(sensorPin);
+  int potValue = analogRead(potPin);
+  int umidade = map(sensorValue, 0, 1023, 0, 100);
+  int delayTime = map(potValue, 0, 1023, 0, 10000);
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Umidade: ");
+  lcd.print(Umidade);
+  lcd.print("%");
+
+  if (humidity < threshold) {
+    myservo.write(servoOpenAngle);
+    delay(servoDelay);
+    myservo.write(servoClosedAngle);
+  } else {
+    myservo.write(servoClosedAngle);
+    delay(delayTime);
+  }
+}
